713715b78aa721c5a3720e7eaabeab89ad0f0354
Maruan Aguerdouh <maruan.mx@gmail.com>
2022-01-09 15:15:28 +0100

    Added winners logic and styling

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -2,7 +2,8 @@ class MovesController < ApplicationController
     def create
         @game = Game.find(params[:game_id])
         @game.move!(params[:row], params[:col])
-    
+        @winner = @game.checkwinner
+        p @winner
         respond_to do |format|
           format.turbo_stream
           format.html {
--- app/models/game.rb
+++ app/models/game.rb
@@ -16,14 +16,75 @@ class Game < ApplicationRecord
   
     # 0, 0
     def move!(row, col)
-      # Make a mark at row/col
-      # {
-      #   0 => { 0 => "x" ...
-      # }
       state[row.to_s][col.to_s] = current_symbol
   
       # swap current symbol
       self.current_symbol = current_symbol == "x" ? "o" : "x"
       self.save!
+    end
+
+    def checkwinner
+        row_winner = checkArrForWinner(rowToArr)
+        col_winner = checkArrForWinner(colToArr)
+        diagonal_winner = checkArrForWinner(diagonalsToArr)
+
+        #Check horizontal combinations
+        return row_winner unless row_winner.nil?
+        #Check vertical combinations
+        return col_winner unless col_winner.nil?
+        #Check diagonal combinations
+        return diagonal_winner unless diagonal_winner.nil?
+        
+
+        return nil
+    end
+    
+    def checkArrForWinner(arr)
+        arr.each do |n|
+            if n.uniq.size <= 1 && n[0] != nil
+                return n[0]
+            end
+        end
+        return nil
+    end
+
+
+    def rowToArr
+        new_arr = [[],[],[]]
+
+        3.times do |r|
+            3.times do |c|
+                new_arr[r] << state[r.to_s][c.to_s]
+            end
+        end
+        new_arr
+    end
+
+    def colToArr
+        new_arr = [[], [], []]
+
+        3.times do |r|
+            3.times do |c|
+                new_arr[r] << state[c.to_s][r.to_s]
+            end
+        end
+        new_arr
+    end
+
+    def diagonalsToArr
+        new_arr = [[], []]
+        aux_index = 2
+
+        2.times do |r|
+            3.times do |c|
+                if aux_index >= 0
+                    new_arr[r] << state[c.to_s][aux_index.to_s]
+                    aux_index -= 1
+               else
+                    new_arr[r] << state[c.to_s][c.to_s]
+               end
+            end
+        end
+        new_arr
     end
   end
